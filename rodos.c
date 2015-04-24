92e85bba687e8a478b3243c5ce949a1519c7bf1f
Will Weber <rwaweber@udel.edu>
2015-04-24 13:35:39 -0400

    adding first sink and source

--- rodos.c
+++ rodos.c
@@ -1,30 +1,3 @@
-
-/*	strong.c
-	Identify strongly connected components in a graph
-
-	by: Steven Skiena
-	begun: March 27, 2002
-*/
-
-/*
-Copyright 2003 by Steven S. Skiena; all rights reserved. 
-
-Permission is granted for use in non-commerical applications
-provided this copyright notice remains intact and unchanged.
-
-This program appears in my book:
-
-"Programming Challenges: The Programming Contest Training Manual"
-by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
-
-See our website www.programming-challenges.com for additional information.
-
-This book can be ordered from Amazon.com at
-
-http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
-
-*/
-
 #include <stdio.h>
 #include "bool.h"
 #include "graph.h"
@@ -43,94 +16,179 @@ int scc[MAXV+1];		/* strong component number for each vertex */
 stack active;			/* active vertices of unassigned component */
 int components_found;		/* number of strong components identified */
 
-process_vertex_early(int v)
+void process_vertex_early(int v)
 {
-	/*printf("entered vertex %d at time %d\n",v, entry_time[v]);*/
+  /*printf("entered vertex %d at time %d\n",v, entry_time[v]);*/
 
-	push(&active,v);
+  push(&active,v);
+  return;
+
 }
 
-process_vertex_late(int v)
+void process_vertex_late(int v)
 {
-        /*printf("exit vertex %d at time %d\n",v, exit_time[v]);*/
+  /*printf("exit vertex %d at time %d\n",v, exit_time[v]);*/
+  if (low[v] == v) {
+    /* edge (parent[v],v) cuts off scc */
+    /*printf("strong commonent started backtracking from %d\n",v);*/
+    pop_component(v);
+  }
+  if (entry_time[low[v]] < entry_time[low[parent[v]]])
+    low[parent[v]] = low[v];
 
-	if (low[v] == v) { 		/* edge (parent[v],v) cuts off scc */
-/*printf("strong commonent started backtracking from %d\n",v);*/
-		pop_component(v);
-	}
-
-	if (entry_time[low[v]] < entry_time[low[parent[v]]])
-		low[parent[v]] = low[v];
+  return;
 }
 
 pop_component(int v)
 {
-        int t;                  /* vertex placeholder */
+  int t;
+  /* vertex placeholder */
 
-	components_found = components_found + 1;
-	printf("%d is in component %d \n",v,components_found);
+  components_found = components_found + 1;
+  printf("%d is in component %d \n",v,components_found);
 
-	scc[ v ] = components_found;
-	while ((t = pop(&active)) != v) {
-		scc[ t ] = components_found;
-		printf("%d is in component %d with %d \n",t,components_found,v);
-	}
+  scc[ v ] = components_found;
+  while ((t = pop(&active)) != v) {
+    scc[ t ] = components_found;
+    printf("%d is in component %d with %d \n",t,components_found,v);
+  }
 }
 
-process_edge(int x, int y)
+int process_edge(int x, int y)
 {
-	int class;		/* edge class */
+  int class;		/* edge class */
 
-	class = edge_classification(x,y);
-/*printf("(%d,%d) class %d\n", x,y,class);*/
+  class = edge_classification(x,y);
+  printf("(%d,%d) class %d\n", x,y,class);
 
-	if (class == BACK) {
-		if (entry_time[y] < entry_time[ low[x] ] )
-			low[x] = y;
-/*printf("process BACK (%d,%d) low[%d]=%d  low[%d]=%d\n",x,y,x,low[x],y,low[y]);*/
-	}
+  if (class == BACK) {
+    if (entry_time[y] < entry_time[ low[x] ] )
+      low[x] = y;
+    return;
+    printf("process BACK (%d,%d) low[%d]=%d  low[%d]=%d\n",x,y,x,low[x],y,low[y]);
+  }
 
-	if (class == CROSS) {
-		if (scc[y] == -1)	/* component not yet assigned */
-			if (entry_time[y] < entry_time[ low[x] ] )
-                        	low[x] = y;
-/*printf("process CROSS (%d,%d) low[%d]=%d  low[%d]=%d\n",x,y,x,low[x],y,low[y]);*/
-	}
+  if (class == CROSS) {
+    if (scc[y] == -1)	/* component not yet assigned */
+      if (entry_time[y] < entry_time[ low[x] ] )
+	low[x] = y;
+    return;
+    printf("process CROSS (%d,%d) low[%d]=%d  low[%d]=%d\n",x,y,x,low[x],y,low[y]);
+  }
 }
 
 
 
-
-
-strong_components(graph *g)
+void strong_components(graph *g)
 {
-	int i;				/* counter */
+  int i;				/* counter */
+  graph simple; //newly created graph of strongly connected components
 
-	for (i=1; i<=(g->nvertices); i++) {
-		low[i] = i;
-		scc[i] = -1;
-	}
+  
+  
+  for (i=1; i<=(g->nvertices); i++)
+    {
+      low[i] = i;
+      scc[i] = -1;
+    }
+  
 
-	components_found = 0;
+  components_found = 0;
 
-	init_stack(&active);
-	initialize_search(&g);
+  init_stack(&active);
+  initialize_search(&g);
 
-	for (i=1; i<=(g->nvertices); i++)
-		if (discovered[i] == FALSE) {
-			dfs(g,i);
-			/*pop_component(i);*/
-		}
+  for (i=1; i<=(g->nvertices); i++)
+    if (discovered[i] == FALSE) {
+      dfs(g,i);
+      
+      /*pop_component(i);*/
+    }
 }
 
-main(int argc, char* argv[])
+
+/* ########################## User created methods ########################## */
+
+//Identify single, cyclic components_found, and then print those same components
+int id_one_component(graph *g)
 {
-	graph g;
-	int i;
-
-	read_graph(&g,TRUE);
-	print_graph(&g);
-
-	strong_components(&g);
+  int i;
+  if (components_found == 1)
+    {
+      printf("Sources:");
+      for(i = 1; i<= (g->nvertices); i++)
+	{
+	  printf("%d", i );
+	}
+      printf("\n");
+      
+      printf("Sponges:");
+      for(i = 1; i<= (g->nvertices); i++)
+	{
+	  printf("%d", i );
+	}
+      printf("\n");
+      return 1;
+    }
+  else
+    return 0;
 }
+
+
+// Identify multiple component graphs, and then determine their sinks and sponges
+int id_mul_component(graph *g)
+{
+  int i, flag = 0; //flag is to indicate if it
+  for(i = 1; i <= (g->nvertices); i++)
+    {
+      
+    }
+}
+
+
+      // End user created methods
+int main(int argc, char* argv[])
+{
+  graph g;
+  int i;
+
+  read_graph(&g,TRUE);
+  print_graph(&g);
+
+  strong_components(&g);
+  
+  if(id_one_component(&g))
+    printf("All of the above vertices are Sources and Sponges because there is only one component.\n");
+  
+      
+  return 0;
+}
+
+
+/*	strong.c
+	Identify strongly connected components in a graph
+
+	by: Steven Skiena
+	begun: March 27, 2002
+*/
+
+/*
+  Copyright 2003 by Steven S. Skiena; all rights reserved. 
+
+  Permission is granted for use in non-commerical applications
+  provided this copyright notice remains intact and unchanged.
+
+  This program appears in my book:
+
+  "Programming Challenges: The Programming Contest Training Manual"
+  by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
+
+  See our website www.programming-challenges.com for additional information.
+
+  This book can be ordered from Amazon.com at
+
+  http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
+
+*/
+
 
